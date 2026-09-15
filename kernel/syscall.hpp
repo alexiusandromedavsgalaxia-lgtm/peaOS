f6eff410d96de833ba7314d3823f833d4d36ac05
FRY Developer <alexiusandromedavsgalaxia@gmail.com>
2026-09-15 21:14:03 +0200

    Define initial stable X90 syscall ABI

--- kernel/syscall.hpp
+++ kernel/syscall.hpp
@@ -1,13 +1,25 @@
 #pragma once
 #include <stdint.h>
 
-namespace syscall {
-enum Number : uint32_t {
-    Yield = 0,
+namespace syscall_api {
+
+enum class Number : uint64_t {
+    Exit = 0,
     Write = 1,
-    Exit = 2,
-    Alloc = 3,
-    Open = 4,
+    GetTime = 2,
+    GetProcessId = 3,
+    GetSystemInfo = 4
 };
-struct Result { int32_t value; int32_t error; };
+
+struct SystemInfo {
+    uint32_t abi_version;
+    uint32_t cpu_count;
+    uint64_t ram_bytes;
+    uint64_t features;
+};
+
+constexpr uint32_t kAbiVersion = 1;
+
+uint64_t dispatch(uint64_t number, uint64_t arg0, uint64_t arg1, uint64_t arg2);
+
 }
