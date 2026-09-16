2be186c654fde005777f13024f3575e1be88fe35
FRY Developer <alexiusandromedavsgalaxia@gmail.com>
2026-09-16 07:41:45 +0200

    drivers: expose PCI BARs and command/status

--- kernel/drivers/pci.hpp
+++ kernel/drivers/pci.hpp
@@ -13,6 +13,10 @@ struct Device {
     uint8_t subclass;
     uint8_t prog_if;
     uint8_t irq_line;
+    uint16_t command;
+    uint16_t status;
+    uint32_t bar[6];
+    bool is_bridge;
 };
 
 constexpr uint32_t kMaxDevices = 256;
@@ -23,5 +27,7 @@ const Device* devices(uint32_t* count);
 uint32_t count();
 uint32_t read32(uint8_t bus, uint8_t slot, uint8_t function, uint8_t offset);
 uint16_t read16(uint8_t bus, uint8_t slot, uint8_t function, uint8_t offset);
+void write32(uint8_t bus, uint8_t slot, uint8_t function, uint8_t offset, uint32_t value);
+void write16(uint8_t bus, uint8_t slot, uint8_t function, uint8_t offset, uint16_t value);
 
 }
